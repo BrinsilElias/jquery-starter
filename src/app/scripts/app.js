6789da495ca9b60a7c1e28aed1270b960d21a9dd
Brinsil Elias <brinsilelias0592@gmail.com>
2024-06-05 18:27:33 +0530

    starter-template: Add prettier and lint-staged. Modified the formatting configuration

--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -1,47 +1,47 @@
 const storageKey = 'theme-preference';
 
 const getColorPreference = () => {
-    if (localStorage.getItem(storageKey)) {
-        return localStorage.getItem(storageKey);
-    }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  if (localStorage.getItem(storageKey)) {
+    return localStorage.getItem(storageKey);
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 };
 
 const theme = {
-    value: getColorPreference(),
+  value: getColorPreference()
 };
 
 const reflectPreference = () => {
-    $(':first-child').attr('data-theme', theme.value);
-    $('#theme-toggle').attr('aria-label', theme.value);
+  $(':first-child').attr('data-theme', theme.value);
+  $('#theme-toggle').attr('aria-label', theme.value);
 };
 
 const setPreference = () => {
-    localStorage.setItem(storageKey, theme.value);
-    reflectPreference();
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
 };
 
 const handleColorSchemeChange = () => {
-    window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .addEventListener('change', ({ matches: isDark }) => {
-            theme.value = isDark ? 'dark' : 'light';
-            setPreference();
-        });
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({ matches: isDark }) => {
+      theme.value = isDark ? 'dark' : 'light';
+      setPreference();
+    });
 };
 
 const onClick = () => {
-    theme.value = theme.value === 'light' ? 'dark' : 'light';
-    setPreference();
+  theme.value = theme.value === 'light' ? 'dark' : 'light';
+  setPreference();
 };
 
 const initThemeToggle = () => {
-    reflectPreference();
-    $('#theme-toggle').on('click', onClick);
+  reflectPreference();
+  $('#theme-toggle').on('click', onClick);
 };
 
 module.exports = {
-    handleColorSchemeChange,
-    reflectPreference,
-    initThemeToggle,
+  handleColorSchemeChange,
+  reflectPreference,
+  initThemeToggle
 };
