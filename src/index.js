45b9a316777460e970617617de54080c46f77fe1
Brinsil Elias <brinsilelias0592@gmail.com>
2024-09-07 11:41:59 +0530

    starter-template: Delete existing files for rsbuild migration

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('./main.scss');
-window.jQuery = window.$ = require('jquery'); // eslint-disable-line no-multi-assign
-
-const run = require('./app/scripts/utilities/run');
-const app = require('./app/scripts/app');
-
-$(() => {
-  run(app);
-});
